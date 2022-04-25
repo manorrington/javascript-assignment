0b7161bb8011394c38d7fea2a32403a14abb2e61
Micah Norrington <manorrington@gmail.com>
2022-04-25 19:56:44 -0400

    added files

---
+++ testing-java/script.js
@@ -0,0 +1,73 @@
+//Very Easy
+// function min(n1, n2){
+//     if (n1 > n2)
+//    return n2
+//     else 
+//    return n1
+// }
+// console.log(min(4,6))
+
+
+
+//Easy
+// let studentName1 = [`John`, `Maria`, `Josh`]
+// let studentName2 = [`Doe`, `Hernandez`, `Center`]
+// let studentAge = [21, 19, 24]
+
+// console.log(`Hello. My name is ${studentName1[0]} ${studentName2[0]} and I'm ${studentAge[0]} years old.`)
+
+
+
+//Medium
+// let birthMonth = [`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`]
+// let month = prompt(`Enter your birth #`)
+
+// if (month === `1`) {
+//     console.log(birthMonth[0])
+// } else if (month === `2`) {
+//     console.log(birthMonth[1])
+// } else if (month === `3`) {
+//     console.log(birthMonth[2])
+// } else if (month === `4`) {
+//     console.log(birthMonth[3])
+// } else if (month === `5`) {
+//     console.log(birthMonth[4])
+// } else if (month === `6`) {
+//     console.log(birthMonth[5])
+// } else if (month === `7`) {
+//     console.log(birthMonth[6])
+// } else if (month === `8`) {
+//     console.log(birthMonth[7])
+// } else if (month === `9`) {
+//     console.log(birthMonth[8])
+// } else if (month === `10`) {
+//     console.log(birthMonth[9])
+// } else if (month === `11`) {
+//     console.log(birthMonth[10])
+// } else if (month === `12`) {
+//     console.log(birthMonth[11])
+// } else {
+//     alert(`Invalid Request. Enter a # between 1-12.`)
+// }
+
+
+
+//Hard //
+// let tomHeight = 9;
+// let tomMass = 8;
+// let jerryHeight = 10;
+// let jerryMass = 45;
+
+// function bmi(n1, n2) {
+//     return n1 / n2 **2;
+// }
+
+// let tomBMI = bmi(tomMass, tomHeight)
+// let jerryBMI = bmi(jerryMass, jerryHeight)
+// console.log(tomBMI, jerryBMI)
+
+// if (tomBMI < jerryBMI) {
+//     console.log(`Jerry's BMI is higher than Tom's`)
+// } else {
+//     console.log(`Tom's BMI is higher than Jerry's`)
+// }
